6c48bc5351ecf4c5dde624670d806a240757e21d
Jose Barranco <jdcoding01@gmail.com>
2019-06-24 01:41:10 -0400

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -5,5 +5,7 @@ const handler = require('../handlers/MainHandler');
 router.get('/', handler.login);
 router.post('/login', handler.auth);
 router.get('/index', handler.index);
+router.get('/signup', handler.signup);
+router.post('/signup', handler.signupAuth);
 
 module.exports = router;
