b355be483c3d9c4e9cfbd92b8694373a7f46696f
Jose Barranco <jdcoding01@gmail.com>
2019-06-24 01:02:56 -0400

    Update users.js

--- database/models/users.js
+++ database/models/users.js
@@ -7,7 +7,7 @@ const UserSchema = new mongoose.Schema({
  psw: {
      type: String
  }
-}, { versionKey: false });
+});
 UserSchema.set('toJSON', { virtuals: true });
 
 const User = mongoose.model('Users', UserSchema);
