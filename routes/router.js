429aa0f2ef604d7e9654ffdf4285cf7acc8cc21b
Jose Barranco <jdcoding01@gmail.com>
2019-06-23 23:40:52 -0400

    Add user auth route.

--- routes/router.js
+++ routes/router.js
@@ -3,6 +3,7 @@ const router = Router();
 const handler = require('../handlers/MainHandler');
 
 router.get('/', handler.login);
+router.post('/', handler.auth);
 router.get('/index', handler.index);
 
 module.exports = router;
