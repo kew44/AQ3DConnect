6652c3a4c7b56eea8fb7e7284416c3b0b61b91f3
Jose Barranco <jdcoding01@gmail.com>
2019-06-24 22:24:49 -0400

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -7,6 +7,6 @@ router.post('/login', handler.auth);
 router.get('/index', handler.index);
 router.get('/signup', handler.signup);
 router.post('/signup', handler.signupAuth);
-
+router.post('/addItem', handler.addItem);
 module.exports = router;
 //export router module
